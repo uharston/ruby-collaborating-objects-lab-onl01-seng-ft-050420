8dba049b1cb93f453be1ec441d92b4e0769a778d
Uriah Harston <uharston@gmail.com>
2020-05-19 19:47:33 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class MP3Importer
 
   attr_accessor :path
@@ -8,6 +9,7 @@ class MP3Importer
 
   def files
     @files ||= Dir.entries(@path).select {|song| !File.directory?(song) && song.end_with?(".mp3")}
+    binding.pry 
   end
 
   def import
